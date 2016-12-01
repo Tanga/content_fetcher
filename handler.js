fce163a3d5434654d4c32d05242f5734d21ce7b8
daniel <danielh714@gmail.com>
2016-12-01 09:26:33 -0800

    make some modules and use different request library

--- handler.js
+++ handler.js
@@ -1,49 +1,14 @@
 'use strict';
-var request = require('request-promise')
-var AWS = require('aws-sdk')
-AWS.config.region = 'us-east-1'
-var s3 = new AWS.S3()
-const uuidV1 = require('uuid/v1')
+
+var request = require('request')
+var handleFetchResult = require('./lib/handle_fetch_result');
 
 module.exports.fetch = (event, context, callback) => {
-  var url = event.url
-  var metadata = event.s3_metadata
-  var bucket = event.s3_bucket
+  var body     = JSON.parse(event.body);
+  var metadata = body.s3_metadata;
+  var bucket   = body.s3_bucket;
 
-  request({uri: url, encoding: null}).then(function(image) {
-    uploadToS3(bucket, image, metadata, function(s3Url) {
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-          s3_url: s3Url,
-          input: event,
-        }),
-      };
-
-      callback(null, response);
-    })
-  })
-
-
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  request(body.url, function(error, res, body){
+    handleFetchResult(res, bucket, metadata, callback);
+  });
 };
-
-function uploadToS3 (bucket, image, metadata, callback) {
-  var params = {
-    Body: image,
-    Bucket: bucket,
-    Key: uuidV1(),
-    Metadata: metadata,
-    ACL: 'public-read',
-    ContentType: image.mime,
-    ContentDisposition: 'inline'
-  }
-
-  const upload = s3.upload(params).promise()
-
-  return upload.then(function(result) {
-    return {url: result.Location}
-  })
-}
