205a5c256166c305243187564a880d29199a9adc
daniel <danielh714@gmail.com>
2016-12-01 09:35:15 -0800

    move function into its own module

--- handler.js
+++ handler.js
@@ -2,13 +2,10 @@
 
 var request = require('request')
 var handleFetchResult = require('./lib/handle_fetch_result');
+var fetchContent = require('./lib/fetch_content')
 
 module.exports.fetch = (event, context, callback) => {
   var body     = JSON.parse(event.body);
-  var metadata = body.s3_metadata;
-  var bucket   = body.s3_bucket;
 
-  request(body.url, function(error, res, body){
-    handleFetchResult(res, bucket, metadata, callback);
-  });
+  fetchContent(body, callback)
 };
